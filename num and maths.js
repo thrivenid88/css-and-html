e9312b6d015a0a473bc3d510f1189bdb18da29d9
unknown <THRIVENI D>
2024-07-08 12:16:39 +0530

    num and maths commited

---
+++ num and maths.js
@@ -0,0 +1,20 @@
+const score=100
+console.log(score)
+
+const balance=new Number(400)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(4))
+console.log(balance.toPrecision())
+
+const otherNumber=2344.6666
+console.log(otherNumber.toPrecision(4))
+
+const hundreds=10000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+console.log(Math.abs(-4))
+console.log(Math.ceil(4.6))
+console.log(Math.round(4.766))
+console.log(Math.sqrt(90000))
